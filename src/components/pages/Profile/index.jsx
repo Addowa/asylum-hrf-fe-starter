a50a6e6da7f536b658bc299ff3b676e80f8fbd18
Addow Abdi <adowabdi007@icloud.com>
2025-10-13 09:48:30 -0500

    wip

--- src/components/pages/Profile/index.jsx
+++ src/components/pages/Profile/index.jsx
@@ -1,15 +1,7 @@
-/**
- * TODO: Ticket 3:
- * Implement authentication using Auth0:
- * - Get the user data from Auth0
- * - Create and style the component
- * - Display the data
- * - Make this page a protected Route
- */
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+
 const Profile = () => {
-  // TODO: Replace these with functionality from Auth0
-  const isLoading = false;
-  const user = true;
+  const { user, isLoading } = useAuth0();
 
   if (isLoading || !user) {
     return <div className='text-center p-4'>Loading...</div>;
