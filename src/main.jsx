d9a439b7d43f3853a0da4d102c13c181c7a90b12
Addow Abdi <adowabdi007@icloud.com>
2025-10-13 10:18:15 -0500

    wip

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 const AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
 const AUTH_CLIENT_ID = import.meta.env.VITE_AUTH_CLIENT_ID;
 
+console.log("Auth Domain:", AUTH_DOMAIN, "Auth Client:", AUTH_CLIENT_ID);
+
 createRoot(document.getElementById('root')).render(
     <Auth0Provider
       domain={AUTH_DOMAIN}
