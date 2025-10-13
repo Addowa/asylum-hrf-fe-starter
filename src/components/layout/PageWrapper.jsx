5163431ff3df68c06cb28e8887aa6143e7ba7183
Addow Abdi <adowabdi007@icloud.com>
2025-10-13 09:35:46 -0500

    wip

--- src/components/layout/PageWrapper.jsx
+++ src/components/layout/PageWrapper.jsx
@@ -3,13 +3,6 @@ import Footer from './Footer.jsx';
 import * as React from 'react';
 
 export const pageWrapper = ele => {
-  // return (
-  //   <>
-  //     <Header />
-  //     {ele}
-  //     <Footer />
-  //   </>
-  // );
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
