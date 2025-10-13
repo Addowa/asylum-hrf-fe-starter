4a31971d591267effc50600208c252bbf78dfb42
Addow Abdi <adowabdi007@icloud.com>
2025-10-13 09:51:49 -0500

    wip

--- src/components/pages/Profile/index.jsx
+++ src/components/pages/Profile/index.jsx
@@ -8,8 +8,18 @@ const Profile = () => {
   }
 
   return (
-    <div>Profile Page</div>
+    <div className='flex flex-col items-center p-8 text-center'>
+      <img
+        src={user?.picture}
+        alt={user?.name}
+        className='w-32 h-32 rounded-full mb-4 border'
+      />
+      <h1 className='text-2xl font-bold mb-2'>{user?.name}</h1>
+      <p className='text-lg'>{user?.email}</p>
+    </div>
   );
 };
 
-export default Profile;
+export default withAuthenticationRequired(Profile, {
+  onRedirecting: () => <div className='text-center p-4'>Redirecting to login...</div>,
+});
